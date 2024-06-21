6041c74b432fa83d4cc28a66c19a3ba0be84bc69
Vicente0709 <stalin.narvaez02@epn.edu.ec>
2024-06-21 18:01:11 -0500

    Seguimiento de Camara a un componente

---
+++ 01-clases/02-proyecto/Carrito/Assets/seguirCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirCamara : MonoBehaviour
+{
+    [SerializeField] private GameObject objSeguir;
+    // Update is called once per frame
+   
+
+    void LateUpdate()
+    {
+        if (objSeguir)
+        {
+            transform.position = objSeguir.transform.position + new Vector3(0, 0, -10);
+        }
+        else
+        {
+        }
+    }
+}
